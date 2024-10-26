1a0e76508d548812f9c2c1be7f548aa4d618efba
Miguel <mmirandasanchez16@gmail.com>
2024-10-26 16:11:22 -0300

    React app ready

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const {dbConnection} = require('./database/config')
 const cors = require('cors')
+const path = require('path')
 
 //create express server 
 const app =  express()
@@ -21,7 +22,9 @@ app.use( express.json())
 //route
 app.use( '/api/auth', require('./routes/auth'))
 app.use( '/api/events', require('./routes/events'))
-
+app.use('*', (req, res) =>{
+    res.sendFile( path.join(__dirname, 'public/index.html') )
+})
 //listener
 app.listen( process.env.PORT, () =>{
     console.log(`server: ${process.env.PORT}`)
